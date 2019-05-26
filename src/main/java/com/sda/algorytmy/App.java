86699cfd78b654d26d17db1966f0836c3a4aeb96
Mateusz Erdman/Matek <mateusz.erdman@hotmail.com>
2019-05-26 15:40:51 +0100

    CSV v2

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,27 @@
 package com.sda.algorytmy;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static Path file = Paths.get("real_estate.csv");
+
+    public static void main(String[] args)
+    { try {
+
+        List<String> lines = Files.readAllLines(file);
+        System.out.println(lines);
+    }catch(IOException e){
+        e.printStackTrace();
+    }
     }
 }
+
